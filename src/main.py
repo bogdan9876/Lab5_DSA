dd2ef98ab8c7785945915c1cf4ac9ddd6bff6675
bogdan9876 <bogdankryveiko@gmail.com>
2023-11-16 19:31:10 +0200

    fix some bugs

--- src/main.py
+++ src/main.py
@@ -9,35 +9,41 @@ def read_txt_file(filename):
 
 
 def possible_combinations_pairs(pairs):
-    first_tribe = set()
-    second_tribe = set()
-    count = 0
+    tribes = [set(pairs[0])]
+    counter = 0
+    union = []
 
-    first_tribe.update(pairs[0])
     for pair in pairs[1:]:
-        if any(member in first_tribe for member in pair):
-            first_tribe.update(pair)
+        for tribe in tribes:
+            if any(member in tribe for member in pair):
+                tribe.update(pair)
+                break
         else:
-            second_tribe.update(pair)
+            tribes.append(set(pair))
 
-    for first_person in first_tribe:
-        for second_person in second_tribe:
-            if (first_person % 2 == 0 and second_person % 2 == 1) or (
-                first_person % 2 == 1 and second_person % 2 == 0
-            ):
-                count += 1
+    tribes_amount = len(tribes)
 
-    return count
+    for first_tribe in range(tribes_amount):
+        for second_tribe in range(first_tribe + 1, tribes_amount):
+            for first_person in tribes[first_tribe]:
+                for second_person in tribes[second_tribe]:
+                    if (first_person % 2 == 0 and second_person % 2 == 1) or (
+                        first_person % 2 == 1 and second_person % 2 == 0
+                    ):
+                        counter += 1
+                        union.append(f"{first_person}/{second_person}")
+
+    return counter, union
 
 
-def generate_output(filename, result):
+def generate_output(filename, result, union):
     with open(filename, "w") as file:
-        file.write(str(result))
+        file.write(f"{result} (Possible pairs - {', '.join(union)})")
 
 
 if __name__ == "__main__":
     input_file = "input.txt"
     output_file = "output.txt"
     pairs = read_txt_file(input_file)
-    result = possible_combinations_pairs(pairs)
-    generate_output(output_file, result)
+    result, union = possible_combinations_pairs(pairs)
+    generate_output(output_file, result, union)
